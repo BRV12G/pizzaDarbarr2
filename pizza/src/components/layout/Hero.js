a99c4c9be25848c0fa139dbe945a662d67acd2e2
BRV12G <159134024+BRV12G@users.noreply.github.com>
2024-12-28 19:23:47 +0530

    adjusted the hero section

--- pizza/src/components/layout/Hero.js
+++ pizza/src/components/layout/Hero.js
@@ -4,17 +4,24 @@ import Right from '@/components/icons/right.js'
 
 export default function Hero() {
     return (
-      <section className="grid grid-cols-2">
+      <section className="grid grid-cols-[0.4fr_0.6fr]">
         <div className='py-12 '>
-           <h1 className="text-4xl font-semibold"> Everything is better with pizza</h1>
-           <p className='my-4 text-gray-500'>Pizza is the missing piece that makes every day complete , a simple yet delicious joy in life</p>
+           <h1 className="text-4xl font-semibold"> 
+              Everything <br />  
+              is better <br /> 
+              with a&nbsp;
+             <span className='text-primary'>
+                Pizza
+             </span>
+            </h1>
+            <p className='my-6 text-gray-500 text-sm'>Pizza is the missing piece that makes every day complete , a simple yet delicious joy in life.</p>
            <div className='flex gap-4 text-sm'>
             <button className= 'bg-primary flex items-center gap-2 text-white px-4 py-2 rounded-full uppercase'>
                 Order Now
                 <Right />
             </button>
-            <button className='flex gap-2 py-2 text-gray-600 font-semibold '>
-                Learn Now
+            <button className='flex items-center gap-2 py-2 text-gray-600 font-semibold '>
+                Learn More
                 <Right />
             </button>
            </div>
