aece2fc7553b54e6c2575c77ec5dc0b99f5aaf45
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-01-08 13:27:05 +0530

    photo for menuitems updated

--- pizza/src/components/menu/MenuItems.js
+++ pizza/src/components/menu/MenuItems.js
@@ -2,7 +2,7 @@ export default function MenuItems() {
     return (
         <div className='bg-gray-200 p-4 rounded-lg text-center group hover:bg-white hover:shadow-md hover:shadow-black/25 transition-all'>
             <div className="text-center">
-               <img src="/pizza.jpg"  alt="pizza1" className="max-h-auto max-h-24 block mx-auto" />
+               <img src="/pizzamenu.png"  alt="pizza1" className="max-h-auto max-h-24 block mx-auto" />
             </div>
             <h4 className='text-xl font-semibold my-3'>Pepperoni Pizza</h4>
             <p className='text-gray-500 text-sm'>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
