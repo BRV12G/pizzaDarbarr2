5f0e77d6ffd896aa503982c8c218fce05febb984
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-01-12 02:58:35 +0530

    logout functionality added

--- pizza/src/components/layout/Header.js
+++ pizza/src/components/layout/Header.js
@@ -1,7 +1,12 @@
-
+"use client";
 import Link from "next/link";
+import {useSession} from 'next-auth/react';
+import {signOut} from 'next-auth/react';
 
 export default function Header() {
+  const session = useSession();
+  console.log(session);
+  const status = session.status;
     return (
       <header className="flex items-center justify-between">
         <nav className="flex items-center gap-8 text-gray-500 font-semibold">
@@ -12,8 +17,23 @@ export default function Header() {
           <Link href=" ">Contact</Link> 
         </nav>
         <nav className="flex items-center gap-4">
-          <Link href={"/login"} className="text-gray-500 font-semibold">Login</Link>
-          <Link href={"/register"} className="bg-primary text-white px-8 py-2 rounded-full">Register</Link> 
+          {status === 'authenticated' && (
+            <button href={"/register"} 
+              onClick={() => signOut()}
+              className="bg-primary text-white px-8 py-2 rounded-full">
+              Logout
+            </button> 
+ 
+          )}
+
+          {status === "unauthenticated" && (
+            <>
+            <Link href={"/login"} className="text-gray-500 font-semibold">Login</Link>
+            <Link href={"/register"} className="bg-primary text-white px-8 py-2 rounded-full">Register</Link> 
+            </>
+          )}
+          {/* <Link href={"/login"} className="text-gray-500 font-semibold">Login</Link> */}
+          {/* <Link href={"/register"} className="bg-primary text-white px-8 py-2 rounded-full">Register</Link>  */}
         </nav>
       </header>
     );
