c7a6202a372e896dc264eea8528cbb6d7bd4bb07
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-01-14 22:56:58 +0530

    feat: profile section form - name and image and user name

--- pizza/src/app/profile/page.js
+++ pizza/src/app/profile/page.js
@@ -2,10 +2,27 @@
 import { useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
 import Image from 'next/image';
+import { useState, useEffect } from "react";
 export default function Profilepage() {
     const session = useSession();
     const {status} = session;
     console.log(session);
+    const [userName , setUsername] = useState('');
+
+    useEffect(() => {
+        if(status === 'authenticated'){
+            setUsername(session.data.user.name);
+        }
+    }, [status , session]);
+    async function handleProfileInfoUpdate(ev) {
+        ev.preventDefault();
+        // console.log(userName);
+        const response = await fetch('/api/profile', {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({name: userName})
+        })
+    }
 
 
     if (status === 'loading'){
@@ -20,7 +37,7 @@ export default function Profilepage() {
     return (
         <section className="mt-8">
             <h1 className="text-4xl text-center text-primary mb-4">Profile</h1>
-            <form className='max-w-md mx-auto'>
+            <div className='max-w-md mx-auto'>
                 <div className='flex gap-4 items-center'>
                     <div>                    
                        <div className='p-2 rounded-lg relative'>
@@ -28,15 +45,16 @@ export default function Profilepage() {
                               <button type="button">Edit</button>
                         </div>
                     </div>
-                    <div className='grow'>
-                        <input type="text" placeholder="First and Last Name" />
+                    <form className='grow' onSubmit={handleProfileInfoUpdate}>
+                        <input type="text" placeholder="First and Last Name" 
+                        value={userName} onChange={ev => setUsername(ev.target.value)}/>
                         <input type="email" value={session.data.user.email} disabled={true}/>
                         <button type='submit'>Save</button>
-                    </div>
+                    </form>
 
                 </div>
 
-            </form>
+            </div>
         </section>
     );
 }
