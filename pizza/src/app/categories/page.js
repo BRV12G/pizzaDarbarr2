3a804423ce92de57fb24ef5a489668691f0f358d
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-01-19 17:09:25 +0530

    data loading of users and fatching it from profile for categories/menu items/users/profile

--- pizza/src/app/categories/page.js
+++ pizza/src/app/categories/page.js
@@ -1,27 +1,36 @@
 "use client";
 import AdminTabs from "@/components/layout/AdminTabs";
+import { useProfile } from "@/components/useProfile";
 import { useEffect } from "react";
 import { useState } from "react";
 
 export default function CategoriesPage() {
-    const [isAdmin, setIsAdmin] = useState(false);
-    const [adminInfoLoading, setAdminInfoLoading] = useState(true);
-    useEffect(() => {
-        setAdminInfoLoading(true);
-        fetch('/api/profile').then(response => {
-            response.json().then(data => {
-                 setIsAdmin(data.admin);
-                 setAdminInfoLoading(false);
-            });
-        })
-    }, []);
+    // const [isAdmin, setIsAdmin] = useState(false);
+    // const [adminInfoLoading, setAdminInfoLoading] = useState(true);
+    // useEffect(() => {
+    //     setAdminInfoLoading(true);
+    //     fetch('/api/profile').then(response => {
+    //         response.json().then(data => {
+    //              setIsAdmin(data.admin);
+    //              setAdminInfoLoading(false);
+    //         });
+    //     })
+    // }, []);
 
-    if (adminInfoLoading) {
+    // if (adminInfoLoading) {
+    //     return 'Loading Admin Info...';
+    // }
+
+    // if(!isAdmin) {
+    //     return 'Not Admin!'
+    // }
+    const {loading:profileLoading, data:profileData} = useProfile();
+    if (profileLoading) {
         return 'Loading Admin Info...';
     }
 
-    if(!isAdmin) {
-        return 'Not Admin!'
+    if(!profileData.admin) {
+        return 'Not Admin!';
     }
     return (
         <section className="mt-8 max-w-lg mx-auto">
