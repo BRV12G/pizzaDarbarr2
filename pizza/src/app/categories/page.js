244326246b0a4818074a3fa4fcfdf25dc4d5c18a
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-01-19 21:12:58 +0530

    categories creation and updation

--- pizza/src/app/categories/page.js
+++ pizza/src/app/categories/page.js
@@ -1,31 +1,57 @@
 "use client";
 import AdminTabs from "@/components/layout/AdminTabs";
 import { useProfile } from "@/components/useProfile";
+import { set } from "mongoose";
 import { useEffect } from "react";
 import { useState } from "react";
-import toast from "react-hot-toast";
+import {toast} from "react-hot-toast";
 
 export default function CategoriesPage() {
    
-    const [newCategoryName, setNewCategoryName] = useState('');
+    const [categoryName, setCategoryName] = useState('');
     const {loading:profileLoading, data:profileData} = useProfile();
-    
-    async function handleNewCategory(ev) {
-        ev.preventDefault();
-        const creationPromise = new Promise(async(resolve, reject) =>{
-            const response = await fetch("/api/categories", {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({name: newCategoryName}),
+    const [categories, setCategories] = useState([]);
+    const [editedCategory , setEditedCategory] = useState(null);
+
+    useEffect(() => {
+        fetchCategories();
+    }, []);
+
+    function fetchCategories() {
+        fetch('/api/categories').then(res => {
+            res.json().then(categories => {
+             setCategories(categories);
             });
+          });
+    }
+    
+    async function handleCategory(ev) {
+        ev.preventDefault();
+        const creationPromise = new Promise(async (resolve, reject) => {
+            const data = {name: categoryName};
+            if(editedCategory) {
+                data._id = editedCategory._id;
+            }
+            const response = await fetch("/api/categories", {
+                method: editedCategory ? 'PUT' : 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(data),
+            });
+            setCategoryName('');
+            fetchCategories();
+            setEditedCategory(null);
             if(response.ok) 
-                resolve()
+                resolve();
             else 
                 reject();
         });
-        toast.promise(creationPromise, {
-            loading: 'Creating Category...',
-            success: 'Category Created Successfully!',
+        await toast.promise(creationPromise, {
+            loading: editedCategory
+             ? 'Updating Category...'
+             : 'Creating Category...',
+            success: editedCategory 
+            ? 'Category Updated Successfully...' 
+            : 'Category Created Successfully!',
             error: 'Error Creating Category'
         });
         
@@ -41,19 +67,39 @@ export default function CategoriesPage() {
     return (
         <section className="mt-8 max-w-md mx-auto">
             <AdminTabs isAdmin = {true} />
-            <form className="mt-8" onSubmit={handleNewCategory}>
+            <form className="mt-8" onSubmit={handleCategory}>
                  <div className="flex gap-2 items-end">
                     <div className="grow">
-                        <label>New Category Name</label>
-                        <input type="text" placeholder="Category Name" value={newCategoryName} onChange={ev => setNewCategoryName(ev.target.value)}/>
+                        <label>
+                            {editedCategory ? "Update Category" : "New Category name"}
+                            {editedCategory && (
+                                <>: <b>{editedCategory.name}</b></>
+                            )}
+                        </label>
+                        <input type="text" placeholder="Category Name" value={categoryName} onChange={ev => setCategoryName(ev.target.value)}/>
                     </div>
                     <div className="pb-2">
-                        <button className="" type="submit">Create</button>
+                        <button className="" type="submit">
+                            {editedCategory ? 'Update' : 'Create'}
+                        </button>
                     </div>
                  </div>
                
          
             </form>
+            <div>
+               <h2 className="mt-8 text-sm text-gray-500">Edit Categories</h2>
+               {categories?.length > 0 && categories.map(c => (
+                <button 
+                    onClick={() => {
+                        setEditedCategory(c);
+                        setCategoryName(c.name);
+                    }}
+                    className="bg-gray-200 rounded-xl p-2 px-4 flex gap-2 mb-1 cursor-pointer">
+                    <span>{c.name}</span>
+                </button>
+               ))}
+            </div>
         </section>
     );
 }
