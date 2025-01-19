c6711a296a9de892a21b9ac35aa9b7f1faad6c58
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-01-20 02:20:02 +0530

    categories connected to mongodb

--- pizza/src/app/api/categories/route.js
+++ pizza/src/app/api/categories/route.js
@@ -1,6 +1,7 @@
 import {Category} from "@/models/Category";
 
 export async function POST(req){
+  mongoose.connect(process.env.MONGO_URL);
   const {name} = await req.json();
   const categoryDoc = await Category.create({name});
   return Response.json(categoryDoc);
@@ -9,12 +10,14 @@ export async function POST(req){
 
 
 export async function PUT(req){
+  mongoose.connect(process.env.MONGO_URL);
   const {_id, name} = await req.json();
   await Category.updateOne({_id}, {name});
   return Response.json(true);
 }
 
 export async function GET(req){
+  mongoose.connect(process.env.MONGO_URL);
   return Response.json(
     await Category.find().lean()
   );
