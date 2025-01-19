1e04077421976518d11e5331d2ded2643bdb8692
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-01-19 17:33:47 +0530

    adding new categories form designed and toasting appiled and function to handle new categories

--- pizza/src/app/categories/page.js
+++ pizza/src/app/categories/page.js
@@ -3,28 +3,34 @@ import AdminTabs from "@/components/layout/AdminTabs";
 import { useProfile } from "@/components/useProfile";
 import { useEffect } from "react";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function CategoriesPage() {
-    // const [isAdmin, setIsAdmin] = useState(false);
-    // const [adminInfoLoading, setAdminInfoLoading] = useState(true);
-    // useEffect(() => {
-    //     setAdminInfoLoading(true);
-    //     fetch('/api/profile').then(response => {
-    //         response.json().then(data => {
-    //              setIsAdmin(data.admin);
-    //              setAdminInfoLoading(false);
-    //         });
-    //     })
-    // }, []);
-
-    // if (adminInfoLoading) {
-    //     return 'Loading Admin Info...';
-    // }
-
-    // if(!isAdmin) {
-    //     return 'Not Admin!'
-    // }
+   
+    const [newCategoryName, setNewCategoryName] = useState('');
     const {loading:profileLoading, data:profileData} = useProfile();
+    
+    async function handleNewCategory(ev) {
+        ev.preventDefault();
+        const creationPromise = new Promise(async(resolve, reject) =>{
+            const response = await fetch("/api/categories", {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({name: newCategoryName}),
+            });
+            if(response.ok) 
+                resolve()
+            else 
+                reject();
+        });
+        toast.promise(creationPromise, {
+            loading: 'Creating Category...',
+            success: 'Category Created Successfully!',
+            error: 'Error Creating Category'
+        });
+        
+    }
+
     if (profileLoading) {
         return 'Loading Admin Info...';
     }
@@ -33,9 +39,21 @@ export default function CategoriesPage() {
         return 'Not Admin!';
     }
     return (
-        <section className="mt-8 max-w-lg mx-auto">
+        <section className="mt-8 max-w-md mx-auto">
             <AdminTabs isAdmin = {true} />
-            categories
+            <form className="mt-8" onSubmit={handleNewCategory}>
+                 <div className="flex gap-2 items-end">
+                    <div className="grow">
+                        <label>New Category Name</label>
+                        <input type="text" placeholder="Category Name" value={newCategoryName} onChange={ev => setNewCategoryName(ev.target.value)}/>
+                    </div>
+                    <div className="pb-2">
+                        <button className="" type="submit">Create</button>
+                    </div>
+                 </div>
+               
+         
+            </form>
         </section>
     );
 }
