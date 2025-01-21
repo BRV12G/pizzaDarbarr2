55f4941c76ebabf10f701ad6d26cb91ef12b7486
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-01-21 23:25:05 +0530

    props ui designed and fuctionality

---
+++ pizza/src/components/layout/menuItemPriceProps.js
@@ -0,0 +1,57 @@
+import Trash from "@/components/icons/Trash";
+import Pluss from "@/components/icons/Pluss";
+
+export default function MenuItemPriceProps({name,addLabel, props, setProps}) {
+
+    // const [sizes, setSizes] = useState([]);
+
+
+    function addProp() {
+        // add a new size to the sizes array
+        setProps(oldProps => {
+          return [...oldProps, { name: "", price: 0 }]
+        });
+
+    }
+
+    function editProp(ev, index, prop){
+      const newValue = ev.target.value;
+      setProps(prevSizes => {
+        const newSizes = [...prevSizes];
+        newSizes[index][prop] = newValue;
+        return newSizes;
+      })
+    }
+
+
+    function removeProp(indexToRemove){
+      setProps(prev => prev.filter((v, index) => index !== indexToRemove))
+    }
+
+
+
+
+    return (
+        <div className="bg-gray-200 p-2 rounded-md mb-2">
+                  <label>{name}</label>
+                  {props?.length > 0 && props.map((size, index) => (
+                    <div className="flex gap-2 items-end">
+                      <div>
+                           <label>Name</label>
+                           <input type="text" placeholder="Size Name" value ={size.name} onChange={ev => editProp(ev, index, 'name')}/>
+                      </div>
+                      <div>
+                          <label>Extra Price</label>
+                           <input type="text" placeholder="Extra Price" value = {size.price} onChange={ev => editProp(ev, index, 'price')}/>
+                      </div>
+                      <div>
+                          <button type="button" className="bg-white mb-2 p-2" onClick ={() => removeProp(index)} > <Trash /> </button>
+                      </div>
+                    </div>
+                    
+                  ))}
+                  <button type="button"  onClick={addProp} className="bg-white"> <Pluss /> {addLabel}</button>
+                </div>
+
+    );
+}
