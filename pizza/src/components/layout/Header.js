f7ddaebffc21efb9698fb60fbfb4cd73ccf9f856
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-01-26 04:12:25 +0530

    cart logo changes

--- pizza/src/components/layout/Header.js
+++ pizza/src/components/layout/Header.js
@@ -3,6 +3,9 @@ import Link from "next/link";
 import {useSession} from 'next-auth/react';
 import {signOut} from 'next-auth/react';
 import Registerpage from "@/app/register/page";
+import { CartContext } from "../AppContext";
+import { useContext } from "react";
+import ShoppingCart from "../icons/shoppingCart";
 
 export default function Header() {
   const session = useSession();
@@ -10,6 +13,7 @@ export default function Header() {
   const status = session?.status;
   const userData = session.data?.user;
   let userName = userData?.name || userData?.email;
+  const {cartProducts} = useContext(CartContext);
   if (userName && userName.includes(' ')) {
     userName = userName.split(' ')[0];
   }
@@ -46,8 +50,10 @@ export default function Header() {
             <Link href={"/register"} className="bg-primary text-white px-8 py-2 rounded-full">Register</Link> 
             </>
           )}
-          {/* <Link href={"/login"} className="text-gray-500 font-semibold">Login</Link> */}
-          {/* <Link href={"/register"} className="bg-primary text-white px-8 py-2 rounded-full">Register</Link>  */}
+            <Link href={'/cart'} className="relative">
+                 <ShoppingCart /> 
+                 <span className="absolute -top-2 -right-2 bg-primary text-white text-xs py-1 px-2 rounded-full leading-3">{cartProducts?.length}</span> 
+            </Link>
         </nav>
       </header>
     );
