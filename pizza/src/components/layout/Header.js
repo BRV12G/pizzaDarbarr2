ea6f6124a514181857af559c71cba17fa4781827
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-01-27 23:19:57 +0530

    fix: cart empty on orders page

--- pizza/src/components/layout/Header.js
+++ pizza/src/components/layout/Header.js
@@ -52,7 +52,9 @@ export default function Header() {
           )}
             <Link href={'/cart'} className="relative">
                  <ShoppingCart /> 
-                 <span className="absolute -top-2 -right-3 bg-primary text-white text-xs py-1 px-2 rounded-full leading-3">{cartProducts?.length}</span> 
+                 {cartProducts?.length > 0 && (
+                    <span className="absolute -top-2 -right-3 bg-primary text-white text-xs py-1 px-2 rounded-full leading-3">{cartProducts?.length}</span> 
+                 )}
             </Link>
         </nav>
       </header>
