15ea3b3a84130e9875e19613323a6aa1654b5b82
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-01-28 02:05:30 +0530

    orders out of admin

--- pizza/src/components/layout/AdminTabs.js
+++ pizza/src/components/layout/AdminTabs.js
@@ -13,9 +13,10 @@ export default function AdminTabs({isAdmin}) {
                         <Link className={path === "/categories" ? "active" : " "} href={'/categories'}>Categories</Link>
                         <Link className={/menu-item/ .test(path) ? "active" : " "} href={'/menu-items'}>Menu Items</Link>
                         <Link className={path.includes('/users') ? "active" : " "} href={'/users'}>Users</Link>
-                        <Link className={path === "/orders" ? "active" : " "} href={'/orders'}>Orders</Link>
                     </>
                 )}
+                <Link className={path === "/orders" ? "active" : " "} href={'/orders'}>Orders</Link>
+
             </div>
     );
 }
