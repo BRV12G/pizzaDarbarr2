c8fb9d2b7a1e50fd08093b331226a01e8f5302c5
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-01-27 23:55:30 +0530

    seperate component for cart product

--- pizza/src/app/cart/page.js
+++ pizza/src/app/cart/page.js
@@ -8,6 +8,7 @@ import AddressInputs from "@/components/layout/AddressInputs";
 import { useState } from "react";
 import { useProfile } from "@/components/useProfile";
 import toast from "react-hot-toast";
+import CartProduct from "@/components/menu/CartProduct";
 // import {createContext} from "react";
 
 
@@ -92,49 +93,7 @@ export default function CartPage() {
                 <div> No Products In Your Cart</div>
                  )}
                  {cartProducts?.length > 0 && cartProducts.map((product, index) => (
-                    <div key={product._id} className="flex gap-4 mb-2 items-center border-b py-2">
-                        {/* <div>
-                            <Image src={product.image} alt= {''} width={240} height={240}/>
-                        </div>  */}
-                        <div className="mb-4 w-24">
-                                    {product.image ? (
-                                        <Image
-                                            src={`data:image/png;base64,${product.image}`} // Use base64 string as src
-                                            alt={product.name || "Product Image"}
-                                            width={200}
-                                            height={200}
-                                        />
-                                    ) : (
-                                        <div className="text-gray-500">
-                                            No Image Available
-                                        </div>
-                                    )}
-                        </div>
-                        <div className="grow">
-                            <h3 className="text-lg font-bold">{product.name}</h3>
-                            {product.size && (
-                                <div className="text-sm">
-                                    Size: 
-                                    <span>{product.size.name}</span>
-                                </div>
-                            )}
-                            {product.extras?.length > 0 && (
-                                <div className="text-sm text-gray-500">
-                                    {product.extras.map(extra => (
-                                        <div key={extra._id}>
-                                            <span>{extra.name} Rs{extra.price}</span>
-                                        </div>
-                                    ))}
-                                </div>
-                            )}
-                        </div>
-                        <div className=" text-lg font-semibold">
-                           Rs.{cartProductPrice(product)}
-                        </div>
-                        <div className="ml-4">
-                            <button type="button" onClick={() => removeCartProduct(index)} className="p-2"> <Trash /> </button>
-                        </div>
-                    </div>
+                    <CartProduct product={product} key={product._id} onRemove={removeCartProduct} />
                  ))}
                  <div className="py-4  pr-16 flex justify-end items-center">
                     <div className="text-gray-500">
---
+++ pizza/src/components/menu/CartProduct.js
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import Trash  from "@/components/icons/Trash";
+import { cartProductPrice } from "@/components/AppContext";
+
+export default function CartProduct(product, onRemove) {
+    return (
+        <div key={product._id} className="flex gap-4 mb-2 items-center border-b py-2">
+                        {/* <div>
+                            <Image src={product.image} alt= {''} width={240} height={240}/>
+                        </div>  */}
+                        <div className="mb-4 w-24">
+                                    {product.image ? (
+                                        <Image
+                                            src={`data:image/png;base64,${product.image}`} // Use base64 string as src
+                                            alt={product.name || "Product Image"}
+                                            width={200}
+                                            height={200}
+                                        />
+                                    ) : (
+                                        <div className="text-gray-500">
+                                            No Image Available
+                                        </div>
+                                    )}
+                        </div>
+                        <div className="grow">
+                            <h3 className="text-lg font-bold">{product.name}</h3>
+                            {product.size && (
+                                <div className="text-sm">
+                                    Size: 
+                                    <span>{product.size.name}</span>
+                                </div>
+                            )}
+                            {product.extras?.length > 0 && (
+                                <div className="text-sm text-gray-500">
+                                    {product.extras.map(extra => (
+                                        <div key={extra._id}>
+                                            <span>{extra.name} Rs{extra.price}</span>
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                        </div>
+                        <div className=" text-lg font-semibold">
+                           Rs.{cartProductPrice(product)}
+                        </div>
+                        <div className="ml-4">
+                            <button type="button" onClick={() => onRemove(index)} className="p-2"> <Trash /> </button>
+                        </div>
+                    </div>
+    );
+}
