66c23a99d6efc8f1bf59b81875354637dcf87c23
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-02-02 22:14:36 +0530

    name updated

--- pizza/src/components/layout/Header.js
+++ pizza/src/components/layout/Header.js
@@ -113,7 +113,7 @@ export default function Header() {
     <header>
       <div className="flex items-center md:hidden justify-between">
         <Link className="text-primary font-semibold text-2xl" href={'/'}>
-          ST PIZZA
+          PIZZA HOUSE
         </Link>
         <div className="flex gap-8 items-center">
           <Link href={'/cart'} className="relative">
