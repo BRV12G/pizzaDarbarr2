21b7cb9d658ac5d5ec65e427833eea11af282734
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-02-04 01:55:08 +0530

    directed links order and learn more buttons

--- pizza/src/components/layout/Hero.js
+++ pizza/src/components/layout/Hero.js
@@ -1,8 +1,12 @@
-
+"use client";
 import Image from 'next/image';
 import Right from '@/components/icons/right.js'
+import { useRouter } from "next/navigation";
+
 
 export default function Hero() {
+   const router = useRouter();
+
     return (
       <section className="hero md:mt-4">
         <div className='md:py-12 py-8'>
@@ -16,11 +20,19 @@ export default function Hero() {
             </h1>
             <p className='my-6 text-gray-500 text-sm'>Pizza is the missing piece that makes every day complete , a simple yet delicious joy in life.</p>
            <div className='flex gap-4 text-sm'>
-            <button className= 'bg-primary flex justify-center items-center gap-2 text-white px-4 py-2 rounded-full uppercase'>
+            {/* <button href ={'/menu'} className= 'bg-primary flex justify-center items-center gap-2 text-white px-4 py-2 rounded-full uppercase'>
                 Order Now
                 <Right />
+            </button> */}
+            <button
+                onClick={() => router.push("/menu")}
+                className="bg-primary flex justify-center items-center gap-2 text-white px-4 py-2 rounded-full uppercase"
+                >
+                Order Now
+               <Right />
             </button>
-            <button className='flex border-0 items-center gap-2 py-2 text-gray-600 font-semibold '>
+            <button className='flex border-0 items-center gap-2 py-2 text-gray-600 font-semibold '
+                onClick={() => router.push("/#about")}>
                 Learn More
                 <Right />
             </button>
