d586bb2441250b6e0beb35bdad12c18e1b5af3f1
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-02-04 01:55:43 +0530

    About us section added real data

--- pizza/src/app/page.js
+++ pizza/src/app/page.js
@@ -9,20 +9,19 @@ export default function Home() {
       <HomeMenu />
       <section className="text-center my-16 " id="about">
         <SectionHeader subHeader={'Our Story'} mainHeader={'About us'}/>
-        <div className=" text-gray-500 max-w-md  mx-auto mt-4 flex flex-col gap-4">
+        <div className=" text-gray-500 max-w-3xl  mx-auto mt-4 flex flex-col gap-4">
             <p className="">
-              lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
-              lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
-              lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            Welcome to FUSION FEAST, where flavors come alive! We bring you a delightful fusion of taste, ambiance, and hospitality, crafting a dining experience like no other. Our menu is carefully curated with fresh ingredients, bold spices, and creative culinary techniques to satisfy every craving.
+            Step into a warm and inviting space, where the aroma of sizzling dishes fills the air, and every meal is made with passion. Whether you're here for a quick bite, a family gathering, or a cozy dinner, we promise delicious flavors and unforgettable moments.
+            Indulge in a variety of dishes, from rich and creamy pastas to zesty wraps, sizzling burgers, and crispy delights. Every bite is a burst of flavor, bringing you comfort and joy. Join us and experience the magic of great food!
 
             </p>
             <p className="">
-              lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
-              lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            From indulgent pastas and sizzling pizzas to hearty burgers and refreshing beverages, we serve a variety of mouthwatering delights. The cozy ambiance, paired with our warm hospitality, makes every visit a memorable experience.
             </p>
             <p>
-               lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
-            </p>
+            Whether you're dining with friends, family, or enjoying a solo treat, we promise an unforgettable culinary journey. Come savor the flavors, enjoy the vibe, and let every meal be a celebration of taste!
+             </p>
         </div>
       </section>
 
